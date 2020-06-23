d3bb960efbcaf87c5af0d87a69df5e93b0550c0d
Wizofgoz <wizofgoz2@gmail.com>
2020-06-22 18:33:18 -0700

    Added to JaegerClientTest

--- tests/JaegerClientTest.php
+++ tests/JaegerClientTest.php
@@ -10,10 +10,12 @@ namespace LaravelOpenTracing\Tests;
 
 use LaravelOpenTracing\Clients\JaegerClient;
 use OpenTracing\Tracer;
+use const Jaeger\SAMPLER_TYPE_PROBABILISTIC;
+use const Jaeger\SAMPLER_TYPE_RATE_LIMITING;
 
 class JaegerClientTest extends TestCase
 {
-    public function testGetTracer()
+    public function testDefaultSampler()
     {
         $client = new JaegerClient([
             'service_name' => 'test.service',
@@ -30,5 +32,61 @@ class JaegerClientTest extends TestCase
         $tracer = $client->getTracer();
 
         $this->assertInstanceOf(Tracer::class, $tracer);
+    }
+
+    public function testProbabilisticSampler()
+    {
+        $client = new JaegerClient([
+            'service_name' => 'test.service',
+            'agent' => [
+                'host' => 'localhost',
+                'port' => 5775,
+            ],
+            'sampler' => [
+                'type' => SAMPLER_TYPE_PROBABILISTIC,
+                'param' => 0.1,
+            ],
+        ]);
+
+        $tracer = $client->getTracer();
+
+        $this->assertInstanceOf(Tracer::class, $tracer);
+    }
+
+    public function testRateLimitingSampler()
+    {
+        $client = new JaegerClient([
+            'service_name' => 'test.service',
+            'agent' => [
+                'host' => 'localhost',
+                'port' => 5775,
+            ],
+            'sampler' => [
+                'type' => SAMPLER_TYPE_RATE_LIMITING,
+                'param' => 10,
+            ],
+        ]);
+
+        $tracer = $client->getTracer();
+
+        $this->assertInstanceOf(Tracer::class, $tracer);
+    }
+
+    public function testInvalidSampler()
+    {
+        $client = new JaegerClient([
+            'service_name' => 'test.service',
+            'agent' => [
+                'host' => 'localhost',
+                'port' => 5775,
+            ],
+            'sampler' => [
+                'type' => 'invalid',
+                'param' => null,
+            ],
+        ]);
+
+        $this->expectException(\RuntimeException::class);
+        $client->getTracer();
     }
 }
